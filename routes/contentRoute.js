91a1572d697a294f22ad7968d4b6f47c52795d68
Dylan Cooper <dylancooper155@gmail.com>
2020-09-29 15:25:12 -0400

    commit after presentation

--- routes/contentRoute.js
+++ routes/contentRoute.js
@@ -3,15 +3,15 @@ const express = require("express");
 const router = express.Router();
 const fs = require("fs");
 const multer = require("multer");
-const storage = multer.diskStorage({
-  destination: function(req, file, cb) {
-      cb(null, 'media/');
-  },
-  filename: function(req, file, cb) {
-      cb(null, file.fieldname + '-' + Date.now() + path.extname(file.originalname));
-  }
-});
-const path = require("path");
+// const storage = multer.diskStorage({
+//   destination: function(req, file, cb) {
+//       cb(null, 'media/');
+//   },
+//   filename: function(req, file, cb) {
+//       cb(null, file.fieldname + '-' + Date.now() + path.extname(file.originalname));
+//   }
+// });
+// const path = require("path");
 const PostsModel = require("../models/contentModel");
 
 
